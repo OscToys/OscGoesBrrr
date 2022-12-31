c9c096998af3858cb9b8dcf737e0649ca7fdbd73
Senky <11907914-senky@users.noreply.gitlab.com>
2022-12-31 07:08:10 -0600

    Enable electron sandboxing, since our renderer process doesn't use any node features

--- src/main/main.ts
+++ src/main/main.ts
@@ -11,6 +11,7 @@ import Buttplug from "./Buttplug";
 import OscConfigDeleter from "./OscConfigDeleter";
 import VrcConfigCheck from "./VrcConfigCheck";
 
+app.enableSandbox();
 process.on("uncaughtException", (err) => {
   dialog.showErrorBox("Fatal Error", err.stack+'');
   app.exit(1);
